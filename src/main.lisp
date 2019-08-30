0f20790ba21ce9243f0fa18c9933fb758678f354
Dan Loaiza <papachan@gmail.com>
2019-08-29 22:07:23 -0500

    catching C-c signal when waiting for input

--- src/main.lisp
+++ src/main.lisp
@@ -3,7 +3,7 @@
 
 (in-package :project-create)
 
-;; setup debug to nil when you create an image
+;; setup debug to nil for release
 (defvar *debug* nil)
 
 (defvar *local-directory*
@@ -28,42 +28,45 @@
               (mustache:render (read-file-string (car n)) values output-stream)
               (format t "~A~%" (second n)))))
 
-;; ask the project name
 (defun main ()
-  "Ask for a project name and create all files"
+  "Ask for a package name and create all files"
   (flet ((prompt (string)
-           (format *query-io* "~a: " string)
-           (force-output *query-io*)
-           (string-trim '(#\space #\tab) (read-line *query-io*))))
-    (let* ((project-name (prompt "What is your project name?"))
+           (let ((*error-output* *query-io*)
+                 (*query-io*     *query-io*))
+             (format t "~%~A " string)
+             (finish-output)
+             (handler-case (string-trim '(#\space #\tab) (read-line *query-io*))
+               (sb-sys:interactive-interrupt ()
+                 (format *error-output* "~%Aborting.~&")
+                 (uiop:quit))))))
+    (let* ((files '())
+           (project-name (prompt "Your package name?"))
            (project-root (make-pathname :directory
-                                        (append (pathname-directory *local-directory*) (list project-name))))
-           (files '()))
-      (progn
-        ;; create directories
-        (loop :for n in '("t" "src")
-              :do (ensure-directories-exist
-                   (make-pathname :directory
-                                  (append (pathname-directory project-root) (list n)))))
-        (unless (directory-exists-p project-root)
-          (error "~S does not exist." project-root))
-
-        ;; make a list from real path and new path file
-        (walk-directory *templates-dir*
-                        (lambda (path)
-                          (let* ((fname (file-namestring path))
-                                 (name (subseq fname 0 (position #\. fname :test #'equal)))
-                                 (subpath (make-pathname :directory
-                                                         (append (list :relative)
-                                                                 (cdr (member "templates" (pathname-directory path) :test #'equal)))
-                                                         :name (mustache:render* name (list (cons :project_name project-name)))
-                                                         :type (subseq fname (1+ (position #\. fname :test #'equal)))))
-                                 (newpath (merge-pathnames subpath project-root)))
-                            (push (list path newpath) files))))
-        (copy-files files (list (cons :project_name project-name)
-                                (cons :year (get-current-year))))
-        (format t ";; DONE~%")))))
-
+                                        (append (pathname-directory *local-directory*)
+                                                (list project-name)))))
+      ;; create directories
+      (loop :for n in '("t" "src")
+            :do (ensure-directories-exist
+                 (make-pathname :directory
+                                (append (pathname-directory project-root)
+                                        (list n)))))
+      (unless (directory-exists-p project-root)
+        (error "~S does not exist." project-root))
+      ;; make a list from real path and new path file
+      (walk-directory *templates-dir*
+                      (lambda (path)
+                        (let* ((fname (file-namestring path))
+                               (name (subseq fname 0 (position #\. fname :test #'equal)))
+                               (subpath (make-pathname :directory
+                                                       (append (list :relative)
+                                                               (cdr (member "templates" (pathname-directory path) :test #'equal)))
+                                                       :name (mustache:render* name (list (cons :project_name project-name)))
+                                                       :type (subseq fname (1+ (position #\. fname :test #'equal)))))
+                               (newpath (merge-pathnames subpath project-root)))
+                          (push (list path newpath) files))))
+      (copy-files files (list (cons :project_name project-name)
+                              (cons :year (get-current-year))))
+      (format t ";; DONE~%"))))
 
 (if *debug*
     (main))
